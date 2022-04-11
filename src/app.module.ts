92b434eb269f23361b7041b99eeea917a70e674d
dogukanakkaya <doguakkaya27@hotmail.com>
2022-04-11 21:07:10 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'CRYPTO_MARKET_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          port: 8081
+        }
+      },
+    ])
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
